4477d029290a3718d48f81266eb0959b33d4df45
Maro2 <Maro2@192.168.0.21>
2020-11-25 17:48:13 +0900

    gradleを理解するため

---
+++ Rennsyuu02/src/main/java/com/example/demo/Rennsyuu02Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rennsyuu02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rennsyuu02Application.class, args);
+	}
+
+}
